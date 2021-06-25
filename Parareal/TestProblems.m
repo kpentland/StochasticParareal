54e7317e0a87c695ecde0e23fc8b78fa87c466f2
Kamran Pentland <55841762+kpentland@users.noreply.github.com>
2021-06-25 10:05:14 +0100

    Efficiency improvements

--- Parareal/TestProblems.m
+++ Parareal/TestProblems.m
@@ -8,26 +8,42 @@ clear; close all; clc
 
 %Inputs:
 f = @(t,u)( sin(u)*cos(u) - 2*u + exp(-t/100)*sin(5*t) + log(1+t)*cos(t) );     %function handle for ODE
-n = 1;                                 %dimension of system
 tspan = [0,100];                        %time interval
-u0 = 1;                                %intial conditions
-N = 40;                                %no. of time sub-intervals steps
-Ndg = 80;                              %no. of coarse steps (in each sub-interval)
-Ndf = Ndg*100;                         %no. of fine steps (in each sub-interval)
-epsilon = 10^(-10);                    %error tolerance 
+u0 = 1;                                 %intial conditions
+N = 40;                                 %no. of time sub-intervals steps
+Ng = 80;                                %no. of coarse steps (in each sub-interval)
+Nf = Ng*100;                            %no. of fine steps (in each sub-interval)
+epsilon = 10^(-10);                     %error tolerance 
 
 %solve with parareal
-[t_fine,U,~,K] = parareal(f,n,tspan,u0,N,Ndg,Ndf,epsilon);
+[t,U,err,K] = parareal(f,tspan,u0,N,Ng,Nf,epsilon);
 
+%solve the ODE in parallel with fine solver using initial conditions from parareal
+dt = (tspan(2)-tspan(1))/Nf;    t_fine = (tspan(1):dt:tspan(2));
 
-%solve using the fine solver serially
-[~,u] = RK(t_fine,u0,f,'classic fourth-order');
+n = length(u0);                       %dimension of system
+dim_indices = (n*(K-1)+1:n*K);        %final solution indices
+U_fine = zeros(Nf+1,length(u0));
+fine_trajecs = cell(N,1);
+parfor i = 1:N
+    [~,temp] = RK((t(i):dt:t(i+1)),U(i,dim_indices),f,'classic fourth-order');
+    if i < N
+    fine_trajecs{i,1} = temp(1:end-1,:);                 
+    else
+    fine_trajecs{i,1} = temp;                 
+    end
+end
+U_fine(1:end,:) = vertcat(fine_trajecs{:,1});
+    
+    
+%solve using the fine solver serially (for comparison)
+[~,u_fine] = RK((tspan(1):dt:tspan(2)),u0,f,'classic fourth-order');
 
 %plot both solutions together
 figure(1)
 hold on
-plot(t_fine,u,'k','LineWidth',2)
-plot(t_fine(1:10:Ndf),U((1:10:Ndf),end),'ob')
+plot(t_fine,u_fine,'k','LineWidth',2)
+plot(t_fine(1:10:Nf+1),U_fine(1:10:Nf+1),'ob')
 xlabel('$t$','Interpreter','latex'); ylabel('$u_1(t)$','Interpreter','latex');
 grid on; box on;
 xlim([0 18]); ylim([-2 2]);
@@ -41,25 +57,41 @@ clear; close all; clc
 
 %Inputs:
 f = @(t,u)([1 + (u(1)^2)*u(2) - (3+1)*u(1); 3*u(1) - (u(1)^2)*u(2)]);     %function handle for ODE
-n = 2;                                 %dimension of system
 tspan = [0,15.3];                      %time interval
 u0 = [1,3.07];                         %intial conditions
 N = 25;                                %no. of time sub-intervals steps
-Ndg = 25;                              %no. of coarse steps (in each sub-interval)
-Ndf = Ndg*100;                         %no. of fine steps (in each sub-interval)
-epsilon = 10^(-6);                    %error tolerance 
+Ng = 25;                               %no. of coarse steps (in each sub-interval)
+Nf = Ng*100;                           %no. of fine steps (in each sub-interval)
+epsilon = 10^(-6);                     %error tolerance 
 
 %solve with parareal
-[t_fine,U,~,K] = parareal(f,n,tspan,u0,N,Ndg,Ndf,epsilon);
+[t,U,err,K] = parareal(f,tspan,u0,N,Ng,Nf,epsilon);
 
-%solve using the fine solver serially
-[~,u] = RK(t_fine,u0,f,'classic fourth-order');
+%solve the ODE in parallel with fine solver using initial conditions from parareal
+dt = (tspan(2)-tspan(1))/Nf;    t_fine = (tspan(1):dt:tspan(2));
 
-%plot both solutions together
+n = length(u0);                       %dimension of system
+dim_indices = (n*(K-1)+1:n*K);        %final solution indices
+U_fine = zeros(Nf+1,length(u0));
+fine_trajecs = cell(N,1);
+parfor i = 1:N
+    [~,temp] = RK((t(i):dt:t(i+1)),U(i,dim_indices),f,'classic fourth-order');
+    if i < N
+    fine_trajecs{i,1} = temp(1:end-1,:);                 
+    else
+    fine_trajecs{i,1} = temp;                 
+    end
+end
+U_fine(1:end,:) = vertcat(fine_trajecs{:,1});
+
+%solve using the fine solver serially (for comparison)
+[~,u_fine] = RK((tspan(1):dt:tspan(2)),u0,f,'classic fourth-order');
+
+%plot both solutions together in 2D phase space
 figure(1)
 hold on
-plot(u(:,1),u(:,2),'k','LineWidth',2)
-plot(U((1:50:Ndf+1),end-1),U((1:50:Ndf+1),end),'ob')
+plot(u_fine(:,1),u_fine(:,2),'k','LineWidth',2)  
+plot(U_fine((1:50:Nf+1),end-1),U_fine((1:50:Nf+1),end),'ob')
 xlabel('$u_1$', 'Interpreter','latex'); ylabel('$u_2$', 'Interpreter','latex');
 axis([0 4 0 5])
 xticks((0:0.5:4))
@@ -75,25 +107,42 @@ clear; close all; clc
 
 %Inputs:
 f = @(t,u)([10*(u(2) - u(1)); 28*u(1) - u(2) - u(1)*u(3); u(1)*u(2) - (8/3)*u(3)]);    %function handle for ODE
-n = 3;                                 %dimension of system
 tspan = [0,18];                        %time interval
 u0 = [-15,-15,20];                     %intial conditions
 N = 50;                                %no. of time sub-intervals steps
-Ndg = 250;                             %no. of coarse steps (in each sub-interval)
-Ndf = Ndg*75;                          %no. of fine steps (in each sub-interval)
-epsilon = 10^(-8);                    %error tolerance 
+Ng = 250;                              %no. of coarse steps (in each sub-interval)
+Nf = Ng*75;                            %no. of fine steps (in each sub-interval)
+epsilon = 10^(-8);                     %error tolerance 
 
 %solve with parareal
-[t_fine,U,~,K] = parareal(f,n,tspan,u0,N,Ndg,Ndf,epsilon);
+[t,U,err,K] = parareal(f,tspan,u0,N,Ng,Nf,epsilon);
 
-%solve using the fine solver serially
-[~,u] = RK(t_fine,u0,f,'classic fourth-order');
+%solve the ODE in parallel with fine solver using initial conditions from parareal
+dt = (tspan(2)-tspan(1))/Nf;    t_fine = (tspan(1):dt:tspan(2));
+
+n = length(u0);                       %dimension of system
+dim_indices = (n*(K-1)+1:n*K);        %final solution indices
+U_fine = zeros(Nf+1,length(u0));
+fine_trajecs = cell(N,1);
+parfor i = 1:N
+    [~,temp] = RK((t(i):dt:t(i+1)),U(i,dim_indices),f,'classic fourth-order');
+    if i < N
+    fine_trajecs{i,1} = temp(1:end-1,:);                 
+    else
+    fine_trajecs{i,1} = temp;                 
+    end
+end
+U_fine(1:end,:) = vertcat(fine_trajecs{:,1});
+
+%solve using the fine solver serially (for comparison)
+[~,u_fine] = RK((tspan(1):dt:tspan(2)),u0,f,'classic fourth-order');
+
 
 %plot the solution in 3D phase space
 figure(1)
 hold on
-plot3(u(:,1),u(:,2),u(:,3),'k','LineWidth',2)
-plot3(U((1:(Ndf/Ndg):Ndf+1),end-2),U((1:(Ndf/Ndg):Ndf+1),end-1),U((1:(Ndf/Ndg):Ndf+1),end),'ob')
+plot3(u_fine(:,1),u_fine(:,2),u_fine(:,3),'k','LineWidth',2)
+plot3(U_fine((1:(Nf/Ng):Nf+1),end-2),U_fine((1:(Nf/Ng):Nf+1),end-1),U_fine((1:(Nf/Ng):Nf+1),end),'ob')
 xlabel('$u_1$', 'Interpreter','latex'); 
 ylabel('$u_2$', 'Interpreter','latex'); 
 zlabel('$u_3$', 'Interpreter','latex');
@@ -108,25 +157,41 @@ clear; close all; clc
 
 %Inputs:
 f = @(t,u)(2*(u/(1+t)) - (t*u)^2);     %function handle for ODE
-n = 1;                                 %dimension of system
 tspan = [0,10];                        %time interval
 u0 = 2;                                %intial conditions
 N = 20;                                %no. of time sub-intervals steps
-Ndg = 20;                              %no. of coarse steps (in each sub-interval)
-Ndf = Ndg*100;                         %no. of fine steps (in each sub-interval)
+Ng = 20;                               %no. of coarse steps (in each sub-interval)
+Nf = Ng*100;                           %no. of fine steps (in each sub-interval)
 epsilon = 10^(-10);                    %error tolerance 
 
 %solve with parareal
-[t_fine,U,~,K] = parareal(f,n,tspan,u0,N,Ndg,Ndf,epsilon);
+[t,U,err,K] = parareal(f,tspan,u0,N,Ng,Nf,epsilon);
 
-%solve using the fine solver serially
-[~,u] = RK(t_fine,u0,f,'classic fourth-order');
+%solve the ODE in parallel with fine solver using initial conditions from parareal
+dt = (tspan(2)-tspan(1))/Nf;    t_fine = (tspan(1):dt:tspan(2));
+
+n = length(u0);                       %dimension of system
+dim_indices = (n*(K-1)+1:n*K);        %final solution indices
+U_fine = zeros(Nf+1,length(u0));
+fine_trajecs = cell(N,1);
+parfor i = 1:N
+    [~,temp] = RK((t(i):dt:t(i+1)),U(i,dim_indices),f,'classic fourth-order');
+    if i < N
+    fine_trajecs{i,1} = temp(1:end-1,:);                 
+    else
+    fine_trajecs{i,1} = temp;                 
+    end
+end
+U_fine(1:end,:) = vertcat(fine_trajecs{:,1});
+
+%solve using the fine solver serially (for comparison)
+[~,u_fine] = RK((tspan(1):dt:tspan(2)),u0,f,'classic fourth-order');
 
 %plot both solutions together
 figure(1)
 hold on
-plot(t_fine,u,'k','LineWidth',2)              %fine solver solution
-plot(t_fine(1:40:Ndf),U((1:40:Ndf),end),'ob') %parareal solution
+plot(t_fine,u_fine,'k','LineWidth',2)              %fine solver solution
+plot(t_fine(1:40:Nf+1),U_fine((1:40:Nf+1),end),'ob') %parareal solution
 xlabel('$t$','Interpreter','latex'); ylabel('$u_1(t)$','Interpreter','latex');
 grid on; box on;
 legend('northeast',{'$\mathcal{F}$ solution','$\mathcal{P}$ solution'},'Interpreter','latex')
@@ -139,25 +204,41 @@ clear; close all; clc
 
 %Inputs:
 f = @(t,u)([-sin(u(1))*(0.1*cos(u(1)) + cos(u(2))); -sin(u(2))*(0.1*cos(u(2)) - cos(u(1)))  ]);     %function handle for ODE
-n = 2;                                 %dimension of system
 tspan = [0,60];                        %time interval
 u0 = [1.5,1.5];                        %intial conditions
 N = 30;                                %no. of time sub-intervals steps
-Ndg = 30;                              %no. of coarse steps (in each sub-interval)
-Ndf = Ndg*100;                         %no. of fine steps (in each sub-interval)
+Ng = 30;                               %no. of coarse steps (in each sub-interval)
+Nf = Ng*100;                           %no. of fine steps (in each sub-interval)
 epsilon = 10^(-8);                     %error tolerance 
 
 %solve with parareal
-[t_fine,U,~,K] = parareal(f,n,tspan,u0,N,Ndg,Ndf,epsilon);
+[t,U,err,K] = parareal(f,tspan,u0,N,Ng,Nf,epsilon);
 
-%solve using the fine solver serially
-[~,u] = RK(t_fine,u0,f,'classic fourth-order');
+%solve the ODE in parallel with fine solver using initial conditions from parareal
+dt = (tspan(2)-tspan(1))/Nf;    t_fine = (tspan(1):dt:tspan(2));
+
+n = length(u0);                       %dimension of system
+dim_indices = (n*(K-1)+1:n*K);        %final solution indices
+U_fine = zeros(Nf+1,length(u0));
+fine_trajecs = cell(N,1);
+parfor i = 1:N
+    [~,temp] = RK((t(i):dt:t(i+1)),U(i,dim_indices),f,'classic fourth-order');
+    if i < N
+    fine_trajecs{i,1} = temp(1:end-1,:);                 
+    else
+    fine_trajecs{i,1} = temp;                 
+    end
+end
+U_fine(1:end,:) = vertcat(fine_trajecs{:,1});
+
+%solve using the fine solver serially (for comparison)
+[~,u_fine] = RK((tspan(1):dt:tspan(2)),u0,f,'classic fourth-order');
 
 %plot both solutions together
 figure(1)
 hold on
-plot(u(:,1),u(:,2),'k','LineWidth',2)
-plot(U((1:60:Ndf+1),end-1),U((1:60:Ndf+1),end),'ob')
+plot(u_fine(:,1),u_fine(:,2),'k','LineWidth',2)
+plot(U_fine((1:60:Nf+1),end-1),U_fine((1:60:Nf+1),end),'ob')
 axis([-0.75 pi+0.75 -0.75 pi+0.75])
 xlabel('$u_1$', 'Interpreter','latex'); ylabel('$u_2$', 'Interpreter','latex');
 xticks((-0.5:0.5:4.5))
--- Parareal/parareal.m
+++ Parareal/parareal.m
@@ -1,71 +1,68 @@
-function [t_fine,u_fine,err,k] = parareal(f,n,tspan,u0,N,Ndg,Ndf,epsilon)
+function [t,u,err,k] = parareal(f,tspan,u0,N,Ng,Nf,epsilon)
 %This function will implement the parareal alogrithm for a system of first
 % order ODEs. This version only updates time sub-intervals that have not
 % converged.
 
 %Inputs:
 % f:           Function handle for function to be solved (i.e. f = @(t,u)([u(1);u(2)])
-% n:           Dimensions of ODE system (i.e. n = 2)
 % tspan:       Time interval over which to integrate (i.e. [0,12])
 % u0:          Initial conditions at tspan(1) (i.e. [0,1])
 % N:           Number of 'proccesors' (temporal sub-intervals) (i.e. N = 40)
-% Ndg:         Number of coarse time steps (i.e. Ndg = 40)
-% Ndf:         Number of fine times steps (i.e. Ndf = 4000)
+% Ng:          Number of coarse time steps (i.e. Ng = 40)
+% Nf:          Number of fine times steps (i.e. Nf = 4000)
 % epsilon:     Error tolerance (i.e. 10^(-10))
 
 %Outputs:
-% t_fine:      Time interval at the fine resolution
-% u_fine:      ODE solution at the fine resolution
-% err:         Error at each time step at each k
+% t:           Vector of time sub-intervals (at which solutions located)
+% u:           Parareal solution to ODE system on the mesh given by 't'
+% err:         Error at each time sub-interval at each k
 % k:           Iterations taken until convergence
 
 %INITIALISATION
 fprintf('Initialising parareal... \n')
 
+n = length(u0);                    %dimension of the ODE system
 L = tspan(2) - tspan(1);           %length of interval
 L_sub = L/N;                       %length of sub-interval
-dt = L/Ndg;                        %coarse time step
-t_sub = (tspan(1):L_sub:tspan(2)); %sub-interval vector
-t_top = t_sub(2:end);              %extra array for parfor loop
-dtt = L/Ndf;                       %fine time step
-t_fine = (tspan(1):dtt:tspan(2));  %fine time interval
+dT = L/Ng;                         %coarse time step
+dt = L/Nf;                         %fine time step
+t = (tspan(1):L_sub:tspan(2));     %time sub-interval vector  
+t_shift = t(2:end);                %shifted time vector for parfor loop below
 
-if mod(Ndg,N)~=0 || mod(Ndf,Ndg)~=0  %number time steps must be multiples of each other
-    fprintf("Ndf must be a multiple of Ndg and Ndg must be a multiple of N")
+if mod(Ng,N)~=0 || mod(Nf,Ng)~=0  %number time steps must be multiples of each other
+    fprintf("Nf must be a multiple of Ng and Ng must be a multiple of N - change time steps")
     return
 end
 
-%The following solution matrices are structured: ith time step by kth iteration
-u_fine = zeros(length(t_fine),n*(N+1)); %final solution matrix at fine resolution
-u = zeros(N+1,n*(N+1));      %stores most accurate up to date solutions (at sub-interval time steps)
+% solution storage matrices (time step x system dimension*iteration)
+u = zeros(N+1,n*(N+1));      %stores parareal solutions (at sub-interval time steps)
 uG = zeros(N+1,n*(N+1));     %stores coarse solutions (at sub-interval time steps)
 uF = zeros(N+1,n*(N+1));     %stores fine solutions (at sub-interval time steps)
-err = zeros(N,N+1);          %error at each time slice
+
+err = zeros(N+1,N);          %error at each time slice
 I = 1;                       %counts how many intervals have converged
 
-%Define the initial condition to be exact at start of each iteration
+% define the initial condition to be exact at start of each iteration
 u(1,:) = repmat(u0,1,N+1);
 uG(1,:) = u(1,:);
 uF(1,:) = u(1,:);
-u_fine(1,:) = u(1,:);
 
 
 %PARAREAL ALGORITHM
 
-%Step 1 (k = 0): Use G (coarse 'classic fourth-order') to find 'rough' initial conditions
+%Step 1 (k = 0): Use G (coarse solver) to find 'rough' initial conditions
 fprintf('Stage 1: Inital coarse solve...')
-[~,temp] = RK(t_sub(1):dt:t_sub(end),u0,f,'classic fourth-order');
-uG(:,1:n) = temp(1:round(L_sub/dt):end,:);
+[~,temp] = RK(t(1):dT:t(end),u0,f,'classic fourth-order');
+uG(:,1:n) = temp(1:round(L_sub/dT):end,:); clear temp;
 u(:,1:n) = uG(:,1:n);
-clear temp
 fprintf('done. \n')
 
 
-%Step 2 (k>0): Use F (finer 'classic fourth-order') in parallel using these initial
-%conditions and update using the predictor-corrector formula.
+%Step 2 (k>0): Use F (fine solver) in parallel using current best initial
+% conditions and update using the predictor-corrector formula.
 for k = 1:N
     
-    % Give an indication as to where we are in the code for the console
+    % give an indication as to which iteration we're at for the console
     if k == 1
         fprintf('Stage 2: Parareal iteration number (out of %.0f): 1 ',N)
     elseif k == N
@@ -74,36 +71,35 @@ for k = 1:N
         fprintf('%.0f ',k)
     end
     
-    %Now we use the previously found (or updated) initial conditions and
-    %solve with the fine solver (in parallel)
+    % use the previously found (or updated) initial conditions and
+    % solve with the fine solver (in parallel)
     dim_indices = (n*(k-1)+1:n*k);        %current indices
     dim_indices_next = ((n*k)+1:n*(k+1)); %next indices
-    fine_trajecs = cell(N,1);
     parfor i = I:N
-        [~,u_f] = RK((t_sub(i):dtt:t_top(i)),u(i,dim_indices),f,'classic fourth-order');
-        uF(i+1,dim_indices) = u_f(end,:);                 %save the solution from final time step in interval
-        fine_trajecs{i,1} = u_f(2:end,:);                 %temporarily store solution in cell array
+        [~,u_f] = RK((t(i):dt:t_shift(i)),u(i,dim_indices),f,'classic fourth-order');
+        uF(i+1,dim_indices) = u_f(end,:);    %save the solution from final time step
     end
-    u_fine(((Ndf/N)*(I-1))+2:end,dim_indices) = vertcat(fine_trajecs{:,1});  %concatenate stored solutions to final output
     
     
     %Predictor-corrector step (for each sub-interval serially)
     for i = I:N
         %First need to find uG for next iteration step using coarse solver
-        [~,u_temp] = RK((t_sub(i):dt:t_sub(i+1)),u(i,dim_indices_next),f,'classic fourth-order');
+        [~,u_temp] = RK((t(i):dT:t(i+1)),u(i,dim_indices_next),f,'classic fourth-order');
         uG(i+1,dim_indices_next) = u_temp(end,:);
         
-        %Do the predic-correc step and save final solution value
+        %Do the predictor-corrector step and save final solution value
         u(i+1,dim_indices_next) = uF(i+1,dim_indices) + uG(i+1,dim_indices_next) - uG(i+1,dim_indices);
     end
     
     %error catch
     if sum(isnan(uG),'all') ~= 0
-        error("NaN values in initial coarse solve - pick more coarse time steps")
+        error("NaN values in initial coarse solve - increase Ng!")
     end
     
-    %Error checking
-    err(k,:) = vecnorm( u(:,dim_indices_next) - u(:,dim_indices), inf, 2);   %error at each time step
+    %%CONVERGENCE CHECKS
+    %if an error is small enough up to a certain time interval, all solutions are saved and the
+    %next k considers only unconverged chunks
+    err(:,k) = vecnorm( u(:,dim_indices_next) - u(:,dim_indices), inf, 2)';   %error at each time step
     
     II = I;
     for p = II:N
@@ -112,15 +108,13 @@ for k = 1:N
             u(p+1,(n*(k+1))+1:end) = repmat(u(p+1,dim_indices_next),1,N-k);
             uG(p+1,(n*(k+1))+1:end) = repmat(uG(p+1,dim_indices_next),1,N-k);
             uF(p+1,(n*k)+1:end) = repmat(uF(p+1,dim_indices),1,N-k+1);
-            u_fine(((Ndf/N)*(p-1))+2:((Ndf/N)*p)+1,(n*k)+1:end) = repmat(u_fine(((Ndf/N)*(p-1))+2:((Ndf/N)*p)+1,dim_indices),1,N-k+1);
             I = I + 1;
         elseif p > II
-            if err(k,p) < epsilon
+            if err(p,k) < epsilon
                 %if further intervals beyond I are converged we can copy solutions to all future k
                 u(p+1,(n*(k+1))+1:end) = repmat(u(p+1,dim_indices_next),1,N-k);
                 uG(p+1,(n*(k+1))+1:end) = repmat(uG(p+1,dim_indices_next),1,N-k);
                 uF(p+1,(n*k)+1:end) = repmat(uF(p+1,dim_indices),1,N-k+1);
-                u_fine(((Ndf/N)*(p-1))+2:((Ndf/N)*p)+1,(n*k)+1:end) = repmat(u_fine(((Ndf/N)*(p-1))+2:((Ndf/N)*p)+1,dim_indices),1,N-k+1);
                 I = I + 1;
             else
                 break
@@ -134,8 +128,8 @@ for k = 1:N
     end
 end
 
-%output the fine solution up to the kth cycle.
-u_fine = u_fine(:,1:n*k);
+%output the matrix containing the solutions after 1,2,3...k iterations
+u = u(:,(n+1):n*(k+1));   
 
 fprintf(' \n')
 fprintf('Step 3: Parareal complete. \n')
